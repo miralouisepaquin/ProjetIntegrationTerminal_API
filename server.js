cbe39ec08d869a226089ee3efdef6b25806d732f
miralouisepaquin <90649621+miralouisepaquin@users.noreply.github.com>
2024-02-22 07:54:34 -0500

    Modification server.js
    modification afin d'implanter Swagger

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+//Service WEB API REST MVC
+//
+//Auteur : Mira Paquin
+//(c)2024 Projet Intégration Terminal
+//
+//Informations
+//  	Installation des modules suivants:
+//    npm install express mongoose cors --save
+//    npm install swagger-ui-express
+//    npm install swagger-jsdoc
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -6,6 +16,37 @@ app.use(cors());
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+
+//Swagger part (documentation)
+const swaggerJSDoc=require('swagger-jsdoc');
+const swaggerUI=require('swagger-ui-express');
+const swaggerOptions={
+    definition:{
+        openapi:'3.0.0',
+        info:{
+            title:'Form API v(1.0)',
+            version:'1.0.0',
+            description:"Forms Api",
+            contact:{
+                name:'Mira Paquin',
+                url:'https://cegeprdl.ca',
+                email:'2065674@cegeprdl.ca'
+            },
+            servers:["http://localhost:3001"]
+        }
+
+    },
+    apis:["./app/routes/*.js"],
+    tags: [
+        {
+            "name": "User",
+            "description": "Endpoints"
+        }
+    ],
+
+}
+const swaggerDocs=swaggerJSDoc(swaggerOptions);
+app.use('/myApiDocs',swaggerUI.serve,swaggerUI.setup(swaggerDocs));
 
 const db = require("./app/models");
 db.mongoose
@@ -20,18 +61,24 @@ db.mongoose
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
+//Route utiliser dans le projet
+var usersRoutes = require('./app/routes/users.routes.js'); 
+var questionsRoutes = require('./app/routes/questions.routes.js');
+var formsRoutes = require('./app/routes/forms.routes.js');
 
-// simple route
-app.get("/", (_, res) => {
-  return res.json({ message: "Bienvenue dans mon application!" });
-});
-require("./app/routes/users.routes.js")(app);
-require("./app/routes/sections.routes.js")(app);
-require("./app/routes/questions.routes.js")(app);
-require("./app/routes/forms.routes.js")(app);
+app.use("/api/users", usersRoutes);
+app.use("/api/questions", questionsRoutes);
+app.use("/api/forms", formsRoutes);
+
+//Route par défaut
+app.get('/',  (req, res) => {
+    res.status(200).send("Bienvenue dans mon App! 🙌");
+  });
+
 // set port, listen for requests
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
+  console.log("Documentation: localhost:"+PORT+"/myApiDocs");
 });
 
